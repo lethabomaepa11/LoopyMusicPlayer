17d345c51b7189a2ff779ce52613a9488e047c9d
Lethabo Maepa <shysnylethabo11@gmail.com>
2024-04-03 02:39:40 +0200

    Add files via upload

---
+++ data/MusicData.js
@@ -0,0 +1,42 @@
+const music = [
+  {
+    id: 1,
+    name: "Jack",
+    artist: "Nasty C",
+    image: "one.jpeg",
+    file: "Nasty_C_-_Jack(256k).mp3"
+    
+  },
+  {
+    id: 2,
+    name: "Lala Ngoxolo",
+    artist: "Ami Faku ft Emtee",
+    image: "two.jpeg",
+    file: "Ami_Faku_-_Lala_Ngoxolo_[Official_Lyric_Video]_-_feat_Emtee.mp3"
+    
+  },
+  {
+    id: 3,
+    name: "Better",
+    artist: "Khalid",
+    image: "three.jpeg",
+    file: "Khalid_-_Better_(Official_Video)(256k).mp3"
+    
+  },
+  {
+    id: 4,
+    name: "Wet Dreamz",
+    artist: "J Cole",
+    image: "four.jpeg",
+    file: "J._Cole_-_Wet_Dreamz_(Video)(256k).mp3"
+    
+  },
+  {
+    id: 5,
+    name: "Ubuhle Bakho",
+    artist: "Ami Faku",
+    image: "five.jpeg",
+    file: "Ubuhle_Bakho(256k).mp3"
+    
+  },
+  ];
